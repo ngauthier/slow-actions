e69c0a9f15cee8d01832724db2f7f3dc3b5044d7
Nick Gauthier <nick@smartlogicsolutions.com>
2009-02-25 17:24:50 -0500

    object orientified and tested

--- lib/slow_actions.rb
+++ lib/slow_actions.rb
@@ -1,7 +1,101 @@
 class SlowActionParser
-  def initialize
-
+  def initialize(file_path)
+    @file_path = file_path
+    raise "File not found: #{file_path}" unless File.exists? file_path
+    @file = File.new(file_path, 'r')
   end
+
+  def parse
+    @actions = []
+    begin
+      while true
+        line = @file.readline
+        if line =~ /^Processing/
+          @actions << parse_action(line)
+        end
+      end
+    rescue EOFError => ex
+      
+    end
+    return @actions
+  end
+
+  def process
+    # store this in a way that we can tell time based on:
+    # controller
+    # controller + action
+    # controller + action + params
+    # user
+    # user + controller
+    # user + controller + action
+    # user + controller + action + params
+    #
+    # and filter by date.
+    @processed_actions = []
+    @actions.each do |a|
+      
+    end
+  end
+
+  def print
+    
+  end
+
+  def print_html
+    raise "Not Implemented"
+  end
+
+  private
+
+  def parse_action(line)
+    action = {}
+    if line =~ /^Processing (\S+)#(\S+) \(for (\S+) at (\S+) (\S+)\) \[(\S+)\]$/
+      action[:controller] = $1
+      action[:action] = $2
+      action[:ip] = $3
+      action[:date] = $4
+      action[:time] = $5
+      action[:method] = $6
+    end
+    line = @file.readline
+    if line =~ /^\s+Session ID: (\S+)$/
+      action[:session_id] = $1
+    end
+    line = @file.readline
+    if line =~ /^\s+Parameters: (.*)$/
+      action[:parameters] = $1
+    end
+    line = @file.readline
+    if line == "\n"
+      return action.merge(parse_error)
+    end
+    @file.readline
+    line = @file.readline
+    if line =~ /^Completed in (\S+)/
+      action[:duration] = $1
+    end
+    if line =~ /Rendering: (\S+)/
+      action[:rendering] = $1
+    end
+    if line =~ /DB: (\S+)/
+      action[:db] = $1
+    end
+    return action
+  end
+
+  def parse_error
+    line = "\n"
+    while line == "\n"
+      line = @file.readline
+    end
+    error_txt = ""
+    while line != "\n"
+      line = @file.readline
+      error_txt += line
+    end
+    {:error_text => error_txt}
+  end
+
 
   def self.parse(file)
     f = File.new(file)
@@ -34,4 +128,5 @@ class SlowActionParser
     end
     return ret
   end
+
 end
--- test/slow_actions_test.rb
+++ test/slow_actions_test.rb
@@ -1,7 +1,20 @@
 require File.dirname(__FILE__) + '/test_helper'
 
 class SlowActionsTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  def setup
+    @log_file = File.join(File.dirname(__FILE__), 'data', 'production.recent.log')
+  end
+
+  def teardown
+    
+  end
+
+  should "be able to read actions from a file" do
+    sap = SlowActionParser.new(@log_file)
+    actions = sap.parse
+    assert actions.size > 0, "No actions returned"
+    assert_equal 16, actions.size, "Wrong number of actions"
+    error_action = actions.detect{|a| a[:error_text] }
+    assert_not_nil error_action, "There should be an error action"
   end
 end
